9a24e1dff7f37ac8148ec0b58bbb1b35b253d8e2
BOBO1997git <yh198595@gmail.com>
2018-10-26 23:19:50 +0900

    added dump function for Type.t

--- min-caml/type.ml
+++ min-caml/type.ml
@@ -1,11 +1,39 @@
 type t = (* MinCamlの型を表現するデータ型 (caml2html: type_t) *)
-  | Unit
-  | Bool
-  | Int
-  | Float
-  | Fun of t list * t (* arguments are uncurried *)
-  | Tuple of t list
-  | Array of t
-  | Var of t option ref
+	| Unit
+	| Bool
+	| Int
+	| Float
+	| Fun of t list * t (* arguments are uncurried *)
+	| Tuple of t list
+	| Array of t
+	| Var of t option ref
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
+
+let rec print_type = function
+	| Unit  -> print_string " Unit "
+	| Bool  -> print_string " Bool "
+	| Int   -> print_string " Int "
+	| Float -> print_string " Float "
+	| Fun (t_list, t) -> 
+		print_string " Fun ( args = (";
+		List.iter print_type t_list;
+		print_string ") (";
+		print_type t;
+		print_string ") "
+	| Tuple t_list -> 
+		print_string " Tuple (";
+		List.iter print_type t_list;
+		print_string ") "
+	| Array t ->
+		print_string " Array (";
+		print_type t;
+		print_string ") "
+	| Var {contents = None} -> 
+		print_string " Var (ref None) "
+	| Var {contents = Some t} -> 
+		print_string " Var (ref Some (";
+		print_type t;
+		print_string ")) "
+
+let print_code t = print_type t; print_newline ()
