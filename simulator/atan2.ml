87dbcbe032fcda8f679aa1589be195bdf787ae1b
BOBO1997git <yh198595@gmail.com>
2019-01-31 19:59:12 +0900

    added samples

---
+++ simulator/atan2.ml
@@ -0,0 +1,2 @@
+let b = 10.0 *. atan 1.0 in (* 8 *)
+print_int (int_of_float b)
--- simulator/read.ml
+++ simulator/read.ml
@@ -2,5 +2,6 @@ let a = create_array 3 3 in
 a.(0) <- read_int ();
 a.(1) <- read_int ();
 a.(2) <- read_int ();
-print_int a.(2);
-print_int a.(1)
+print_int a.(0);
+print_int a.(1);
+print_int a.(2)
