85b137975d97a634ef23353cc3e44955c8fc87ab
BOBO1997git <yh198595@gmail.com>
2019-02-04 22:45:04 +0900

    fixed cos.ml

--- simulator/cos.ml
+++ simulator/cos.ml
@@ -1,8 +1,8 @@
 let a = 10.0 *. cos 0.0 in (* 1 *)
-let b = 10.0 *. cos 1.0 in (* -4 *)
-let c = 10.0 *. cos (-. 1.0) in (* -4 *)
-let d = 10.0 *. cos 5.0 in (* 3 *)
-let e = 10.0 *. cos (-. 5.0) in (* 3 *)
+let b = 10.0 *. cos 1.0 in (* 5 *)
+let c = 10.0 *. cos (-. 1.0) in (* 5 *)
+let d = fabs (10.0 *. cos 5.0) in (* 3 *)
+let e = fabs (10.0 *. cos (-. 5.0)) in (* 3 *)
 print_int (int_of_float a);
 print_char 10;
 print_int (int_of_float b);
--- simulator/sin.ml
+++ simulator/sin.ml
@@ -2,7 +2,7 @@ let a = 10.0 *. sin 0.0 in (* 0 *)
 let b = 10.0 *. sin 1.0 in (* 8 *)
 let c = fabs (10.0 *. sin (-. 1.0)) in (* -8 *)
 let d = fabs (10.0 *. sin 5.0) in (* -10 *)
-let e = 10.0 *. sin (-. 5.0) in (* 10 *)
+let e = fabs (10.0 *. sin (-. 5.0)) in (* 10 *)
 print_int (int_of_float a);
 print_char 10;
 print_int (int_of_float b);
