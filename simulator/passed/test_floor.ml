0339233b7a21abeca1d0c497f3889efa41cc8f97
BOBO1997git <yh198595@gmail.com>
2019-02-06 17:15:40 +0900

    added test_floor.ml

---
+++ simulator/passed/test_floor.ml
@@ -0,0 +1,28 @@
+print_int (int_of_float (floor 12.0));
+print_char 10;
+print_int (int_of_float (floor 12.1));
+print_char 10;
+print_int (int_of_float (floor 12.3));
+print_char 10;
+print_int (int_of_float (floor 12.5));
+print_char 10;
+print_int (int_of_float (floor 12.7));
+print_char 10;
+print_int (int_of_float (floor 12.9));
+print_char 10;
+print_int (int_of_float (fabs (floor -12.0)));
+print_char 10;
+print_int (int_of_float (fabs (floor -12.1)));
+print_char 10;
+print_int (int_of_float (fabs (floor -12.3)));
+print_char 10;
+print_int (int_of_float (fabs (floor -12.5)));
+print_char 10;
+print_int (int_of_float (fabs (floor -12.7)));
+print_char 10;
+print_int (int_of_float (fabs (floor -12.9)));
+print_char 10;
+print_int (int_of_float (fabs (floor 0.0)));
+print_char 10;
+print_int (int_of_float (fabs (floor -0.0)));
+print_char 10
